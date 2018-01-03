1a01590912b193763adb5efdd6c9827d669578af
satker <satker73@gmail.com>
2018-01-03 13:51:05 +0300

    push

--- src/main/java/com/epam/laba/onlinestore/controller/Application.java
+++ src/main/java/com/epam/laba/onlinestore/controller/Application.java
@@ -1,4 +1,13 @@
 package com.epam.laba.onlinestore.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Application {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "Hello world";
+    }
 }
